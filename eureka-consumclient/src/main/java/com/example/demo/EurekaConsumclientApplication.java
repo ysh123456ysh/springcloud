926faa00edb43cd9de019c73de002730bc20ba0f
yinshihua <1419658367@qq.com>
2019-03-25 20:17:25 +0800

    duanluqicommit

--- eureka-consumclient/src/main/java/com/example/demo/EurekaConsumclientApplication.java
+++ eureka-consumclient/src/main/java/com/example/demo/EurekaConsumclientApplication.java
@@ -6,12 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 @EnableDiscoveryClient
+@EnableHystrix //开启断路器
 public class EurekaConsumclientApplication {
 
 	public static void main(String[] args) {
--- eureka-consumclient/src/main/java/com/example/demo/service/impl/ConsumServiceImpl.java
+++ eureka-consumclient/src/main/java/com/example/demo/service/impl/ConsumServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.service.ConsumService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -20,7 +21,13 @@ public class ConsumServiceImpl implements ConsumService{
     }
 
     @Override
+    @HystrixCommand(fallbackMethod = "consumServiceError")// 断路器配置，当无法调用如下方法时，就会调用自定的hiError方法。
     public String hiService( String name ) {
         return  restTemplate.getForObject("http://EUREKA-PRODUCTION/index/hi?name=" + name, String.class);
+    }
+
+    public String consumServiceError(String name){
+        return "hey " +
+                name + ", there is some problem with hi page";
     }
 }
